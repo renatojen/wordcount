35b73ce057a5a9a3f0efe8fb93ad01d1b523b6e6
Renato Jensen Filho <rjensen@br.ibm.com>
2020-02-11 15:22:24 -0300

    Adding project files, updated README

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# wordcount
+# Wordcount
+
+You can try a live version of Wordcount here: https://wordcount.renatojensen.com/
+
+## Requirements
+
+Runtime:
+
+* python3
+
+Python packages:
+
+* Click==7.0
+* Flask==1.1.1
+* itsdangerous==1.1.0
+* Jinja2==2.11.1
+* MarkupSafe==1.1.1
+* Werkzeug==1.0.0
+
+##Usage
+
+Input words and click the "Count!" button. The page will reload itself, displaying the number of words found.
+
+## Run the app locally
+
+To run the app locally you will need:
+
+* python3 runtime
+* the app source code
+
+1. Clone wordcount repository
+  ```
+  $ git clone https://github.com/renatojen/wordcount.git
+  ```
+  This repository contains all necessary files to run the app locally.
+
+2. Install the required dependencies with pip: in wordcount project folder, run the following command in a command line:
+  ```
+  $ pip install -r requirements.txt
+  ```
+  (you might need to use `pip3` command depending on your environment configuration)
+
+3. Run the application:
+  ```
+  $ python wordcount.py
+  ```
+  (you might need to use `python3` command depending on your environment configuration)
+  
+This command will create a new Flask app and start your application. When your app has started, your console will print the following message:
+
+`Running on http://localhost:5000/ (Press CTRL+C to quit)`.
+
+4. Test you application acessing http://localhost:5000/ on your browser.
+
+## Compatibility
+Wordcount is compatible with the majority of popular browsers such as Google Chrome, Mozilla Firefox, Opera and mobile devices that supports HTML5.
+   
+## License
+This app is covered by the MGNU GENERAL PUBLIC license. For more information, see LICENSE for license information.
---
+++ wordcount.py
@@ -0,0 +1,71 @@
+"""
+* 
+* wordcount.py
+* Author:
+* Renato Jensen Filho
+* 2020-02-11
+* 
+"""
+
+#imports
+from flask import Flask, render_template, request
+#from os import remove, listdir, getenv
+#from os.path import join, dirname
+from datetime import datetime
+#from time import time
+import re
+
+dt = str(datetime.now())[:-5].replace(' ', '_').replace(':', '').replace('.', '') #YYYY-mm-DD_HHMMSSS   
+vr = None
+ts = None
+
+def count_words(s):
+  count = 0
+  #print(s)
+  #s = re.sub(r'[ .,/?|:;~˜!@#$%^ˆ&*(){}\'`_=+\-\"\[\]\\\n\r\t]+', ' ', s)
+  s = re.sub(r'[^A-Za-z0-9]+', ' ', s)
+  #print(s)
+  s = s.strip(' ')
+  #print(s)
+  if len(s) == 0:
+    return 0
+  for i in s:
+    if i == ' ':
+      count += 1
+  return count+1
+
+#s = " uma \n duas \r tres \t quatro.cinco]seis\\sete/oito,nove.dez?onze!doze#treze$catorze@quize\%dezesseis^dezessete*dezoito&dezenove(vinte)vinteum-vintedois_vintetres+vintequatro=vintecinco"
+#print(count_words(s))
+ 
+#initialization
+#def init():
+#   cfg = safe_load(open("cfg.yml"))
+   
+
+#init()
+    
+#
+# Flask routines
+#
+
+#initializes flask application      
+application=Flask(__name__)
+
+@application.route('/')
+def index():        
+   return render_template("index.html", label=" ")
+  
+#classify image, display image and classification results, generate audio results and save image data to database
+@application.route('/', methods=['POST'])
+def count():
+   if request.method == 'POST':
+      text_input = str(request.form['text_input'])
+              
+
+   return render_template('index.html', label='Words counted: ' + str(count_words(text_input)))           
+
+if __name__=="__main__":
+   # Bind to VC_APP PORT/HOST if defined, otherwise default to 5000/localhost.
+   #PORT = int(getenv('VCAP_APP_PORT', '5000'))
+   #HOST = str(getenv('VCAP_APP_HOST', 'localhost'))
+   application.run()
